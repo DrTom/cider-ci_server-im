e7ed11a1409c4549bd8da3f6ba15ef7a34a7a62c
Thomas Schank <DrTom@schank.ch>
2014-05-10 19:35:59 +0200

    Listen explicitly to 0.0.0.0 in dev mode

--- bin/start-polyglot.sh
+++ bin/start-polyglot.sh
@@ -2,4 +2,4 @@ TORQUEBOX_HOME=/Users/thomas/.immutant/current \
 JBOSS_HOME=$TORQUEBOX_HOME/jboss \
 JRUBY_HOME=/Users/thomas/.rbenv/versions/jruby-1.7.11 \
 JAVA_OPTS="-Xms64m -Xmx4G -XX:MaxPermSize=512m" \
-lein immutant run
+lein immutant run -b '0.0.0.0'
